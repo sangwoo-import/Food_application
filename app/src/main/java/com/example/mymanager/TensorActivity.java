621d459962ee96b3a2bc65089e0ef0f5280cdae0
sangwoo-import <tkddn000000@naver.com>
2022-05-04 13:28:11 +0900

    새로 액티비티 설정해서 만드는게 나을듯

--- app/src/main/java/com/example/mymanager/TensorActivity.java
+++ app/src/main/java/com/example/mymanager/TensorActivity.java
@@ -32,13 +32,12 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 
-
+// 모델을 읽어오는 함수로, 텐서플로 라이트 홈페이지에 있다.
+// MappedByteBuffer 바이트 버퍼를 Interpreter 객체에 전달하면 모델 해석을 할 수 있다.
 
 public class TensorActivity  extends AppCompatActivity {
-    private Button btnCapture;
-    private ImageView imgCapture;
-    private static final int Image_Capture_Code = 1;
-
+    private static final int FROM_ALBUM = 1;    // onActivityResult 식별자
+    private static final int FROM_CAMERA = 2;   // 카메라는 사용 안함
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,86 +45,86 @@ public class TensorActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tensor);
 
-        btnCapture =(Button)findViewById(R.id.btnTakePicture);
-        imgCapture = (ImageView) findViewById(R.id.capturedImage);
-        btnCapture.setOnClickListener(new View.OnClickListener() {
+        // 인텐트의 결과는 onActivityResult 함수에서 수신.
+        // 여러 개의 인텐트를 동시에 사용하기 때문에 숫자를 통해 결과 식별(FROM_ALBUM 등등)
+        findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cInt,Image_Capture_Code);
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");                      // 이미지만
+                intent.setAction(Intent.ACTION_GET_CONTENT);    // 카메라(ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, FROM_ALBUM);
             }
         });
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 카메라를 다루지 않기 때문에 앨범 상수에 대해서 성공한 경우에 대해서만 처리
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Image_Capture_Code) {
-            if (resultCode == RESULT_OK) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                bp = RotateBitmap(bp, 90);
-                int cx = 128, cy = 128;
-                bp = Bitmap.createScaledBitmap(bp, cx, cy, false);
-                int[] pixels = new int[cx * cy];
-                bp.getPixels(pixels, 0, cx, 0, 0, cx, cy);
+        if (requestCode != FROM_ALBUM || resultCode != RESULT_OK)
+            return;
 
-                ByteBuffer input_img = getInputImage(pixels, cx, cy);
-                Interpreter tf_lite = getTfliteInterpreter("output-11.tflite");
+        //각 모델에 따른 input , output shape 각자 맞게 변환
+        // mobilenetcheck.h5 일시 224 * 224 * 3
 
-                float[][] pred = new float[1][2];
-                tf_lite.run(input_img, pred);
+        float[][][][] input = new float[1][224][224][3];
+        float[][][] output = new float[1][12543][4]; //tflite에 버섯 종류 5개라서 (내기준)
 
-                final String predText = String.format("%f", pred[0][0]) + String.format("%f", pred[0][1]);
-                if (pred[0][0] > pred[0][1]){
-                    Toast toast = Toast.makeText(getApplicationContext(), String.format("김치일 확률: %f", pred[0][0]), Toast.LENGTH_LONG);
-                    toast.show();
-                }else{
-                    Toast toast = Toast.makeText(getApplicationContext(), String.format("밥일 확률 : %f", pred[0][1]), Toast.LENGTH_LONG);
-                    toast.show();
+        try {
+
+            int batchNum = 0;
+            InputStream buf = getContentResolver().openInputStream(data.getData());
+            Bitmap bitmap = BitmapFactory.decodeStream(buf);
+            buf.close();
+
+            //이미지 뷰에 선택한 사진 띄우기
+            ImageView iv = findViewById(R.id.image);
+            iv.setScaleType(ImageView.ScaleType.FIT_XY);
+            iv.setImageBitmap(bitmap);
+
+
+            // x,y 최댓값 사진 크기에 따라 달라짐 (조절 해줘야함)
+            for (int x = 0; x < 224; x++) {
+                for (int y = 0; y < 224; y++) {
+                    int pixel = bitmap.getPixel(x, y);
+                    input[batchNum][x][y][0] = Color.red(pixel) / 1.0f;
+                    input[batchNum][x][y][1] = Color.green(pixel) / 1.0f;
+                    input[batchNum][x][y][2] = Color.blue(pixel) / 1.0f;
                 }
-
-
-                Log.d("prediction", predText);
-
-
-                imgCapture.setImageBitmap(bp);
-
-
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             }
-        }
-    }
 
+             //자신의 tflite 이름 써주기
+              Interpreter lite = getTfliteInterpreter("output224.tflite");
 
-
-
-
-
-    public static Bitmap RotateBitmap(Bitmap source, float angle)
-    {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        Bitmap temp = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-        temp = ThumbnailUtils.extractThumbnail(temp, 1080, 1080);
-
-        return temp;
-    }
-    // 다루기 편한 1차원 배열 사용
-    private ByteBuffer getInputImage(int[] pixels, int cx, int cy) {
-        ByteBuffer input_img = ByteBuffer.allocateDirect(cx * cy * 3 * 4);
-        input_img.order(ByteOrder.nativeOrder());
-
-        for (int i = 0; i < cx * cy; i++) {
-            int pixel = pixels[i];        // ARGB : ff4e2a2a
-
-            input_img.putFloat(((pixel >> 16) & 0xff) / (float) 255);
-            input_img.putFloat(((pixel >> 8) & 0xff) / (float) 255);
-            input_img.putFloat(((pixel  >>0) & 0xff) / (float) 255);
+            lite.run(input,output); //output
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return input_img;
+        TextView tv_output = findViewById(R.id.tv_output);
+        int i;
+
+        // 텍스트뷰에 무슨 버섯인지 띄우기 but error남 ㅜㅜ 붉은 사슴뿔만 주구장창
+        for (i = 0; i <4; i++) {
+            if (output[0][0][i] * 100 > 30) {
+                if (i == 0) {
+                    tv_output.setText(String.format("bg  %d %.5f", i, output[0][1][0] * 100));
+                } else if (i == 1) {
+                    tv_output.setText(String.format("kim  %d  %.5f", i, output[0][1][1] * 100));
+                } else if (i == 2) {
+                    tv_output.setText(String.format("kimchi %d, %.5f", i, output[0][1][2] * 100));
+                }
+                  else if (i == 3) {
+                  tv_output.setText(String.format("bab, %d, %.5f", i, output[0][1][3] * 100));
+               }
+            else {
+                 tv_output.setText(String.format("아무 음식, %d, %.5f", i));
+                 //output[0][0][4] * 100)
+               }
+            } else
+                continue;
+        }
     }
 
 
@@ -140,8 +139,7 @@ public class TensorActivity  extends AppCompatActivity {
         return null;
     }
 
-    // 모델을 읽어오는 함수로, 텐서플로 라이트 홈페이지에 있다.
-    // MappedByteBuffer 바이트 버퍼를 Interpreter 객체에 전달하면 모델 해석을 할 수 있다.
+
     private MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException {
         AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
         FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
